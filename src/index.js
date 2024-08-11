4758e87454536b5f5f5ddc2651d9090e9829f892
monicakodeeswaran32@gmail.com <monicakodeeswaran32@gmail.com>
2024-08-11 23:33:10 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Gallery  from "./Gallery"
-
+import "./style.css"
 const root=ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<div><h1 style={{textAlign:"center", fontSize:"3rem"}}>Favourite cartoon</h1>
+root.render(<div><h1 className="header">Favourite cartoon</h1>
 <Gallery></Gallery></div>)
